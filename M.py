c1230c09ec9fc29f0572ed4a874bc8f0a9d56bd2
MicahMurr <micahmurrayaustin@gmail.com>
2026-04-02 16:30:42 -0500

    made turtles

---
+++ M.py
@@ -0,0 +1,93 @@
+from turtle import *
+
+screen=Screen()
+
+
+
+screen.title("Write your name")
+screen.bgcolor("teal")
+screen.setup(width=1000,height=400)
+
+# yertle=Turtle()
+
+# yertle.color("turquoise")
+# yertle.shape("turtle")
+
+# screen.exitonclick()
+
+# """"
+# Movement 
+# """
+# # for j in range(4):
+# #     for i in range(5):
+# #         yertle.forward(10)
+# #         yertle.penup()
+# #         yertle.forward(10)
+# #         yertle.pendown()
+# #     yertle.left(90)
+
+
+
+
+# # while True:
+# #     angle=int(input("What heading would you yertle to face?"))
+# #     yertle.setheading(angle)
+
+
+# """
+# Movement - Coordinates
+# """
+# bess=Turtle()
+# bess.pu()
+# bess.color("red")
+# bess.goto(100,-100)
+
+
+
+# yertle.pu()
+# yertle.goto(-100,100)
+# yertle.pd()
+# yertle.sety(100)
+# yertle.setx(100)
+# yertle.goto(bess.position())
+# yertle.goto(-100,-100)
+# yertle.home()
+
+#for i in range(180):
+#      yertle.forward(1)
+#     yertle.right(1)
+# # yertle.circle(-50,180)
+
+a=Turtle()
+
+a.color("turquoise")
+a.shape("turtle")
+for i in range(180):
+    a.forward(5)
+    a.right(5)
+
+screen.exitonclick()
+
+b=Turtle()
+
+b.color("turquoise")
+b.shape("turtle")
+
+c=Turtle()
+
+c.color("turquoise")
+c.shape("turtle")
+
+d=Turtle()
+
+d.color("turquoise")
+d.shape("turtle")
+
+e=Turtle()
+
+e.color("turquoise")
+e.shape("turtle")
+
+
+turtle.pensize(2)
+ONITT
